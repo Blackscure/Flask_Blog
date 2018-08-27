aac36d9a702bac26372ecd6d528e1702305d197b
Blackscure <Wekesabuyahi@gmail.com>
2018-08-27 16:46:26 +0300

    my commits

--- src/ flask_blog.py
+++ src/ flask_blog.py
@@ -1,5 +1,10 @@
 from flask import Flask,render_template, url_for
+from forms import RegistrationForm, LoginForm
+
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'Blackcure'
 
 posts = [
     {
@@ -23,6 +28,16 @@ def hello():
 @app.route("/about")
 def about():
     return render_template("about.html", title='About')
+
+@app.route("/register")
+def register():
+    return render_template("register.html", title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title='Login', form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
