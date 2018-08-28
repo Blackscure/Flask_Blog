540aaa41a25ce93478e950a8741fb42319e2fdeb
Blackscure <Wekesabuyahi@gmail.com>
2018-08-28 13:34:20 +0300

    my commits

--- src/flask_blog.py
+++ src/flask_blog.py
@@ -34,7 +34,7 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+        if form.email.data == 'test@test.com' and form.password.data == 'test':
             flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
