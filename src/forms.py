4cbe35179a352f752452cbaa8489a389edc2e91f
Blackscure <Wekesabuyahi@gmail.com>
2018-08-28 12:15:57 +0300

    my commits

--- src/forms.py
+++ src/forms.py
@@ -1,27 +1,24 @@
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired,Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
-                            validators=[DataRequired(), Length(min=2,max=20)])
+                           validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
-                        validators=[DataRequired(), Email()])                        
-
-    password = PasswordField('PasswordField', validators=[DataRequired()])
-    cornfirm_password = PasswordField('cornfirm Password',
-                                        validators=[DataRequired(), EqualTo('password')])
-
-    submit = SubmitField('sign Up')
-
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
-                        validators=[DataRequired(), Email()])                        
-
-    password = PasswordField('PasswordField', validators=[DataRequired()])
-    remember = BooleanField('_Remember Me_')
-    submit = SubmitField('_Login_')
-
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
 
